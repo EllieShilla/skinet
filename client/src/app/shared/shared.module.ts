864fe13851ab2104bb349c081aa7f54350f34a0d
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2022-12-22 11:38:50 +0200

    Client Identity

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,16 +5,28 @@ import { PaginHeaderComponent } from './components/pagin-header/pagin-header.com
 import { PagerComponent } from './components/pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
-  declarations: [PaginHeaderComponent, PagerComponent, OrderTotalsComponent],
-  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  declarations: [PaginHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot(),
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   exports: [
     PaginationModule,
     PaginHeaderComponent,
     PagerComponent,
     CarouselModule,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    BsDropdownModule,
+    TextInputComponent
   ],
 })
 export class SharedModule {}
